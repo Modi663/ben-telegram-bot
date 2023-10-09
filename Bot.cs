a0ae006e3e6d0ea86f6b7ddacfae1157fc3ec4d8
Modi663 <berbeca.vadik67@gmail.com>
2023-10-10 00:07:37 +0300

    добавление готового проекта

---
+++ Bot.cs
@@ -0,0 +1,108 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using File = System.IO.File;
+
+
+namespace BenTelegramBot
+{
+    class Bot
+    {
+        // Объявляем константы
+        private static DialogHandler DialogHandler = new DialogHandler();
+        private static ITelegramBotClient bot = new TelegramBotClient(File.ReadAllText("../../../TOKEN.txt"));
+
+        private static List<string> pathList = new List<string>
+        {
+            "../../../resourses/f1.mp4",
+            "../../../resourses/f2.ogg",
+            "../../../resourses/f4.ogg",
+            "../../../resourses/f5.ogg",
+            "../../../resourses/f6.ogg",
+            "../../../resourses/f7.ogg",
+            "../../../resourses/f8.ogg"
+        };
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, // receive all update types
+            };
+
+            bot.StartReceiving(
+                BotMessageHandler,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+                );
+            Console.ReadLine();
+        }
+
+
+        static async Task BotMessageHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {   
+            // Собирание логов в консоль
+            Console.WriteLine($"Update Id: {update.Id}, Username: {update?.Message?.Chat.Username}, Message: {update?.Message?.Text}, Date: {update?.Message?.Date}");
+            Random random = new Random();
+
+            if (update?.Type == UpdateType.Message)
+            {
+                var message = update?.Message;
+                switch (message?.Text?.ToLower())
+                {
+                    case "/start":
+                        await botClient.SendTextMessageAsync(message.Chat,"Привет, привет. Я пёс Ден. Не Бен, не надо путать, мне это не нравится.");
+                        return;
+                    case "/call":
+                        await botClient.SendVideoNoteAsync(message.Chat, System.IO.File.OpenRead("../../../resourses/f3.mp4"));
+                        DialogHandler.AddDialog(message.Chat.Id, true);
+                        return;
+                    default:
+                    {
+                        // Получаем объект
+                        var PATH = pathList[random.Next(pathList.Count)];
+                        await using var stream = System.IO.File.OpenRead(PATH);
+                        if (message != null && DialogHandler.inDialog(message.Chat.Id))
+                        {
+                            if (PATH == pathList[0]) { DialogHandler?.RemoveDialog(message.Chat.Id); }
+
+                            Console.WriteLine($"Responce: {PATH}\n");
+                            switch (Path.GetExtension(PATH))
+                            {
+                                case ".ogg":
+                                    await botClient.SendVoiceAsync(message.Chat, voice: stream);
+                                    return;
+                                case ".mp4":
+                                    await botClient.SendVideoNoteAsync(message.Chat, videoNote: stream);
+                                    return;
+                            }
+                        }
+                    } 
+                    break;
+                }
+            }
+        }
+
+        private static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception,
+            CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+            
+
+    }
+}
---
+++ DialogHandler.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using Telegram.Bots.Types;
+using File = System.IO.File;
+
+namespace BenTelegramBot
+{
+    internal class DialogHandler
+    {
+        private static string path = "../../../dialoges.json";
+        public void AddDialog(long key, bool value)
+        {
+            List<KeyValuePair<long, bool>> keyValuePairs = new List<KeyValuePair<long, bool>> { new (key, value) };
+            string updatedJsonString = JsonSerializer.Serialize(keyValuePairs);
+            File.WriteAllText(path, updatedJsonString);
+            Console.WriteLine($"Диалог {key} добавлен");
+        }
+
+        public async Task RemoveDialog(long key)
+        {
+            string jsonString = File.ReadAllText(path);
+
+            if (!string.IsNullOrEmpty(jsonString))
+            {
+                List<KeyValuePair<long, bool>>? keyValuePairs = JsonSerializer.Deserialize<List<KeyValuePair<long, bool>>>(jsonString);
+
+                bool foundKey = false;
+                for (int i = 0; i < keyValuePairs?.Count; i++)
+                {
+                    if (keyValuePairs[i].Key == key)
+                    {
+                        foundKey = true;
+                        keyValuePairs[i] = new KeyValuePair<long, bool>(key, false);
+                        break;
+                    }
+                }
+                if (foundKey)
+                {
+                    string updatedJsonString = JsonSerializer.Serialize(keyValuePairs);
+                    File.WriteAllText(path, updatedJsonString);
+                    Console.WriteLine($"Диалог {key} удален");
+                }else { Console.WriteLine($"Ключ {key} не найден."); }
+            }else { Console.WriteLine("Файл пуст или не найден."); }
+        }
+
+        public bool inDialog(long key)
+        {
+            string jsonString = File.ReadAllText(path);
+            if (!string.IsNullOrEmpty(jsonString))
+            {
+                List<KeyValuePair<long, bool>>? keyValuePairs = JsonSerializer.Deserialize<List<KeyValuePair<long, bool>>>(jsonString);
+                bool foundValue = false;
+                if (keyValuePairs != null)
+                    foreach (KeyValuePair<long, bool> kvp in keyValuePairs)
+                    {
+                        if (kvp.Key == key)
+                        {
+                            foundValue = kvp.Value;
+                            break;
+                        }
+                    }
+
+                return foundValue;
+            }else { return false; }
+        }
+    }
+}
